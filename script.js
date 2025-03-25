7781ad09a0bd1e9012b12af516a132cfdbbbd3b7
Haneef-Dudekula <dudekulahaneef2301@gmail.com>
2025-03-26 03:21:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// write js code here if required
+// function time() {
+	const date = new Date();
+	
+	const day = date.getDate();
+	const month = date.getMonth()+1; //months starts with 0 so we add 1
+	const year = date.getFullYear();
+
+	const hours = date.getHours().toString();
+	const mins = date.getMinutes().toString();
+	const secs = date.getSeconds().toString();
+
+	const time = `${day}/${month}/${year}, ${hours}:${mins}:${secs}`;
+
+	document.getElementById("timer").textContent =  time;
+	
+	
+// }
+// time()
